e8f318ffa2561d232ddb8a1000f3ad0625f6bc69
mghcool <mghcool@163.com>
2023-07-28 15:23:32 +0800

    增加了全局异常捕获

--- SerialPortToNet/App.xaml.cs
+++ SerialPortToNet/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace SerialPortToNet
@@ -10,6 +12,27 @@ namespace SerialPortToNet
     {
         public App()
         {
+            this.DispatcherUnhandledException += (sender, e) =>
+            {
+                // 处理UI线程上的未处理异常
+                MessageBox.Show(e.Exception.Message, "UI线程异常");
+                e.Handled = true;
+                this.Shutdown();
+            };
+            AppDomain.CurrentDomain.UnhandledException += (sender, e) =>
+            {
+                // 处理非UI线程上的未处理异常
+                Exception exception = (Exception)e.ExceptionObject;
+                MessageBox.Show(exception.Message, "非UI线程异常");
+                this.Shutdown();
+            };
+            TaskScheduler.UnobservedTaskException += (sender, e) =>
+            {
+                // 处理异步异常
+                MessageBox.Show(e.Exception.Message, "非UI线程异常");
+                e.SetObserved();
+                this.Shutdown();
+            };
             // 注册编码
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
         }
